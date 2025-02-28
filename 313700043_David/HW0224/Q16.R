fb13fde0b43b4a4ff83997480ca47dd7b3b9e355
DavidLu1208 <leoluoffical@gmail.com>
2025-02-28 18:47:22 +0800

    Update Q16.R

--- 313700043_David/HW0224/Q16.R
+++ 313700043_David/HW0224/Q16.R
@@ -1,40 +1,28 @@
 temp_file <- tempfile(fileext = ".rdata")
 
-
 download.file(url = "https://www.principlesofeconometrics.com/poe5/data/rdata/capm5.rdata", 
               destfile = temp_file, 
-              mode = "wb")  
+              mode = "wb")
 
 load(temp_file)
 capm5
 
-
-
 firms <- c("ge", "ibm", "ford", "msft", "dis", "xom")
-market_col <- "mkt"
-riskfree_col <- "riskfree" 
-
+market_col <- "mkt"  
+riskfree_col <- "riskfree"
 
 capm5$MKT_excess <- capm5[[market_col]] - capm5[[riskfree_col]]
 
-
 results <- data.frame(Firm = firms, Beta = numeric(length(firms)))
 
-
 for(i in 1:length(firms)) {
-  
   firm_excess <- capm5[[firms[i]]] - capm5[[riskfree_col]]
-  
-
   model <- lm(firm_excess ~ capm5$MKT_excess)
-  
-  
   results$Alpha[i] <- coef(model)[1]
   results$Beta[i] <- coef(model)[2]
 }
 
 print(results)
-
 
 defensive_firm <- results$Firm[which.min(results$Beta)]
 defensive_beta <- min(results$Beta)
@@ -42,27 +30,22 @@ defensive_beta <- min(results$Beta)
 aggressive_firm <- results$Firm[which.max(results$Beta)]
 aggressive_beta <- max(results$Beta)
 
-
 cat(defensive_firm, "Beta值 =", round(defensive_beta, 4), "\n")
 cat(aggressive_firm, "Beta值 =", round(aggressive_beta, 4), "\n")
 
-# 計算 Microsoft 的超額報酬
 msft_excess <- capm5$msft - capm5$riskfree
 
-# 取出市場超額報酬
 market_excess <- capm5$MKT_excess
 
-# 建立線性回歸模型
 model_msft <- lm(msft_excess ~ market_excess)
 
-# 繪製散點圖
 plot(market_excess, msft_excess,
      main = "CAPM for Microsoft",
      xlab = "Market Excess Return (R_m - R_f)",
      ylab = "MSFT Excess Return (R_MSFT - R_f)",
      pch = 19,       # 點的形狀
      col = "blue")   # 點的顏色
-# 加入回歸直線
+
 abline(model_msft, col = "red", lwd = 2)
 
 
@@ -71,7 +54,7 @@ alpha_msft <- results$Alpha[4]
 beta_msft <- results$Beta[4]
 print(alpha_msft)
 print(beta_msft)
-# 計算 Microsoft 的超額報酬（如果尚未在資料框內）
+
 capm5$msft_excess <- capm5$msft - capm5$riskfree
 
 ggplot(capm5, aes(x = MKT_excess, y = msft_excess)) +
@@ -88,8 +71,7 @@ ggplot(capm5, aes(x = MKT_excess, y = msft_excess)) +
 
 results_no_intercept <- data.frame(
   Firm = firms,
-  Beta = numeric(length(firms))  # 預先放置 Beta (無截距) 的欄位
-)
+  Beta = numeric(length(firms))) 
 for (i in seq_along(firms)) {
   
   # 公司超額報酬 (R_i - R_f)
@@ -111,7 +93,7 @@ print(results_no_intercept)
 #  - Beta_no_intercept: 無截距CAPM的Beta
 #  - Alpha: 有截距CAPM中的Alpha
 #  - Firm: 公司名稱
-results <- data.frame(
+results_compare <- data.frame(
   Firm = firms,
   Alpha = numeric(length(firms)),
   Beta_with_intercept = numeric(length(firms)),
@@ -129,17 +111,17 @@ for (i in seq_along(firms)) {
   model_with_int <- lm(firm_excess ~ capm5$MKT_excess)
   
   # 截距(Alpha) & Beta
-  results$Alpha[i]              <- coef(model_with_int)[1]
-  results$Beta_with_intercept[i] = coef(model_with_int)[2]
+  results_compare$Alpha[i]              <- coef(model_with_int)[1]
+  results_compare$Beta_with_intercept[i] = coef(model_with_int)[2]
   
   ## (B) 無截距CAPM
   # (R_i - R_f) = β (R_m - R_f)
   model_no_int <- lm(firm_excess ~ 0 + capm5$MKT_excess)
   
   # 無截距模型只有1個係數，存為 Beta
-  results$Beta_no_intercept[i] = coef(model_no_int)[1]
+  results_compare$Beta_no_intercept[i] = coef(model_no_int)[1]
 }
 
 # 查看結果
-results$Beta_diff <- results$Beta_with_intercept - results$Beta_no_intercept
-print(results)
+results_compare$Beta_diff <- results$Beta_with_intercept - results$Beta_no_intercept
+print(results_compare)
