23e6b6fcb0cdee782e63bbf8bbcc3b4b749d4567
ChangHungCheng <chang50513@gmail.com>
2025-03-21 04:03:50 +0800

    Add files via upload

---
+++ 413707003_Jack/HW0317/HW0317Q2.r
@@ -0,0 +1,33 @@
+# 4.4
+# d
+# (i)
+# marginal effect(10y) = 15.312 / 10 = 1.5312
+
+# (ii)
+# marginal effect(20y) = 15.312 / 20 = 0.7656
+
+
+# e
+# Model 2's R^2 is higher than Model 1's, even when Model 1 is estimated using only the technical artists with some experience, so Model 2 fits the data better.
+
+
+# f
+# Model 2 is more reasonable because it has the property of diminishing marginal returns, where the impact of early experience growth is greater.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
