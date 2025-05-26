e33e1258ce014b94acbbdd7063e1440c270ef00b
11mei <yimei19990320@gmail.com>
2025-05-26 18:15:32 +0800

    Add files via upload

---
+++ 313707031_Megan/HW0512/313707031_HW0519_15.17.R
@@ -0,0 +1,20 @@
+library(POE5Rdata)
+data(liquor5)
+
+
+#b
+library(plm)
+pdata <- pdata.frame(liquor5, index = c("hh", "year"))
+re_model <- plm(liquor ~ income, data = pdata, model = "random")
+
+summary(re_model)
+confint(re_model)
+
+#c
+plmtest(plm(liquor ~ income, data = pdata, model = "pooling"), type = "bp")
+
+#d
+liquor5$income_mean <- ave(liquor5$income, liquor5$hh, FUN = mean)
+pdata$income_mean <- ave(pdata$income, pdata$hh, FUN = mean)
+cre_model <- plm(liquor ~ income + income_mean, data = pdata, model = "random")
+summary(cre_model)
